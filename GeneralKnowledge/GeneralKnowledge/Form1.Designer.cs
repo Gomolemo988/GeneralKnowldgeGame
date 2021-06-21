92a08b43eb27db2d42c16bb849a7236b43ace799
Gomolemo988 <71825048+Gomolemo988@users.noreply.github.com>
2021-06-21 22:25:16 +0200

    Add files via upload

---
+++ GeneralKnowledge/GeneralKnowledge/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace GeneralKnowledge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestions = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestions
+            // 
+            this.lblQuestions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestions.Location = new System.Drawing.Point(138, 227);
+            this.lblQuestions.Name = "lblQuestions";
+            this.lblQuestions.Size = new System.Drawing.Size(518, 51);
+            this.lblQuestions.TabIndex = 1;
+            this.lblQuestions.Text = "Question";
+            this.lblQuestions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnsEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(298, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnsEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(90, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(272, 53);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnsEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(402, 373);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(298, 53);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnsEvent);
+            // 
+            // picBox
+            // 
+            this.picBox.Image = global::GeneralKnowledge.Properties.Resources.succulent;
+            this.picBox.Location = new System.Drawing.Point(90, 4);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(610, 220);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestions);
+            this.Controls.Add(this.picBox);
+            this.Name = "Form1";
+            this.Text = "General Knowldge Game";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label lblQuestions;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ GeneralKnowledge/GeneralKnowledge/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneralKnowledge
+{
+    public partial class Form1 : Form
+    {
+        int correctAns;
+        int qnumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions; 
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            askQuestion(qnumber);
+            totalQuestions = 4; //no. of qs in the game 
+
+        }
+
+        private void checkAnsEvent(object sender, EventArgs e)
+        {
+            //evrytime the buttons are clicked, this event iwll be triggered//check which obj sent this event
+            var senderObje = (Button)sender;
+            int buttontag = Convert.ToInt32(senderObje.Tag);//will convert  the int and send it to this btton tag
+            if (buttontag==correctAns)
+            {
+                score++;
+
+            }
+            if (qnumber==totalQuestions)
+            {
+                //work out percentage
+                percentage = (int)Math.Round((double)(score * 100 / totalQuestions));
+                MessageBox.Show("Quiz ended" + Environment.NewLine + " you answered" + " " + score + " " + "questions correctly" +
+                    Environment.NewLine + "" + "" + "Total Percentage: " + "" + percentage + "%" + Environment.NewLine + "click ok to play again");
+                score = 0;
+                qnumber = 0;
+                askQuestion(qnumber);
+            
+            }
+            qnumber++;
+            askQuestion(qnumber);
+        }
+          private void askQuestion(int qnum)//question number
+        {
+            //function that is going to run evrytime you ask a question
+            // when the statement is false it will continue u is true
+            switch (qnum)
+            {
+                case 1:
+                    picBox.Image = Properties.Resources.succulent;//name of the image in the picture box 
+                    lblQuestions.Text = "What type of plant is this";
+                    button1.Text = "Rose";
+                    button2.Text = "Succulent";// correct answer
+                    button3.Text = "Lilly";
+                    button4.Text = "Daisie";
+                    correctAns = 2;
+                    break;
+
+                case 2:
+                    picBox.Image = Properties.Resources.drake;//name of the image in the picture box 
+                    lblQuestions.Text = "Whose ablum is this?";
+                    button1.Text = "Drake";// correct answer 
+                    button2.Text = "Rick Ross";
+                    button3.Text = "JMSN";
+                    button4.Text = "Kid Cudi";
+                    correctAns = 1;
+                    break;
+
+                case 3:
+                    picBox.Image = Properties.Resources.bird;//name of the image in the picture box 
+                    lblQuestions.Text = "What type of bird is this?";
+                    button1.Text = "Raven";
+                    button2.Text = "Tweety";
+                    button3.Text = "Robin";
+                    button4.Text = "Columbidae";//correct answer
+                    correctAns =4 ;
+                    break;
+
+                case 4:
+                    picBox.Image = Properties.Resources.controller;//name of the image in the picture box 
+                    lblQuestions.Text = "To which company does this controller belong to?";
+                    button1.Text = "Nintendo";
+                    button2.Text = "Hisense";
+                    button3.Text = "Sony";// correct answer 
+                    button4.Text = "Samsung";
+                    correctAns = 3;
+                    break;
+                default:
+                    Console.WriteLine("Looks like we are facing a problem. We're working on it... ");
+                    break;
+            }
+        }
+    }
+}
